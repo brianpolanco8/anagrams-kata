706c652dc1999332f34849e23a7a205ade2d9254
brianpolanco <brian@applab.do>
2020-12-19 23:43:14 -0400

    remove unnecesary comments

--- anagrams.test.js
+++ anagrams.test.js
@@ -4,21 +4,12 @@ const anagrams = require("./anagrams");
 let array = [];
 
 beforeAll(async (done) => {
-  // Do whatever you need to do
-  // fs.readFile("./wordlist.txt", function (err, data) {
-  //   if (err) throw err;
-  //   const words = data.toString().split("\n");
-
-  //   console.log("words", words);
-  // });
-
   array = fs.readFileSync("./wordlist.txt").toString().split("\n");
 
   done();
 });
 
 describe("should work", () => {
-  //   console.log(list);
   test("should work", () => {
     expect(anagrams(array).count).toEqual(20683);
   });
