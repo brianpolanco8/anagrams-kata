58600a4cb8221140ff44812ddc79bca91323e072
brianpolanco <brian@applab.do>
2020-12-19 23:16:03 -0400

    implemented anagrams method

---
+++ anagrams.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+var anagrams = function (wordlist) {
+  const cache = {};
+  wordlist.forEach((str) => {
+    const key = str.split("").sort().join("").trim();
+
+    if (cache[key]) {
+      cache[key].push(str);
+    } else {
+      cache[key] = [str];
+    }
+  });
+
+  // to get the quantity of anagrams
+  return Object.values(cache).filter((el) => el.length > 1).length;
+
+  // to get all anagrams
+  // return Object.values(cache).filter((el) => el.length > 1);
+};
+
+fs.readFile("./wordlist.txt", function (err, data) {
+  if (err) throw err;
+  var wordslist = data.toString().split("\n");
+
+  console.log(anagrams(wordslist));
+});
